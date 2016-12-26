83ff1a974487c37c4db218a04559eb9295681824
amansingh61 <amansingh.61ds@gmail.com>
2016-12-26 15:17:01 +0530

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-var captcha = require("captcha-parser");
-var fs = require("fs");
-var buf = fs.readFileSync("captcha.bmp");
-
-var pixMap = captcha.getPixelMapFromBuffer(buf);
-
-console.log(captcha.getCaptcha(pixMap));
